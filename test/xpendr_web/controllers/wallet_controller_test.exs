cf1055cd62fd0ac069a30c7b235f332a715f0ed3
João Fernandes <joaofnds@joaofnds.com>
2020-02-22 13:12:54 -0300

    Fix wallet_controller_test
    - use ExMachina
    - use user tags

--- test/xpendr_web/controllers/wallet_controller_test.exs
+++ test/xpendr_web/controllers/wallet_controller_test.exs
@@ -1,34 +1,19 @@
 defmodule XpendrWeb.WalletControllerTest do
   use XpendrWeb.ConnCase, async: true
 
-  alias Xpendr.Finance
-
   @create_attrs %{balance: 42, description: "some description", name: "some name"}
   @update_attrs %{balance: 43, description: "some updated description", name: "some updated name"}
   @invalid_attrs %{balance: nil, description: nil, name: nil}
 
-  def fixture(:user) do
-    insert(:user)
-  end
-
-  def fixture(:wallet) do
-    user = fixture(:user)
-
-    {:ok, wallet} =
-      @create_attrs
-      |> Map.merge(%{user_id: user.id})
-      |> Finance.create_wallet()
-
-    wallet
-  end
-
   describe "index" do
+    @tag :as_inserted_user
     test "lists all wallets", %{conn: conn} do
       conn = get(conn, Routes.wallet_path(conn, :index))
       assert html_response(conn, 200) =~ "Listing Wallets"
     end
   end
 
+  @tag :as_inserted_user
   describe "new wallet" do
     test "renders form", %{conn: conn} do
       conn = get(conn, Routes.wallet_path(conn, :new))
@@ -37,12 +22,13 @@ defmodule XpendrWeb.WalletControllerTest do
   end
 
   describe "create wallet" do
+    @tag :as_inserted_user
     test "redirects to show when data is valid", %{conn: conn} do
       conn =
         post(
           conn,
           Routes.wallet_path(conn, :create),
-          wallet: Map.merge(@create_attrs, %{user_id: fixture(:user).id})
+          wallet: Map.merge(@create_attrs, %{user_id: build(:user).id})
         )
 
       assert %{id: id} = redirected_params(conn)
@@ -52,6 +38,7 @@ defmodule XpendrWeb.WalletControllerTest do
       assert html_response(conn, 200) =~ "Show Wallet"
     end
 
+    @tag :as_inserted_user
     test "renders errors when data is invalid", %{conn: conn} do
       conn = post(conn, Routes.wallet_path(conn, :create), wallet: @invalid_attrs)
       assert html_response(conn, 200) =~ "New Wallet"
@@ -61,6 +48,7 @@ defmodule XpendrWeb.WalletControllerTest do
   describe "edit wallet" do
     setup [:create_wallet]
 
+    @tag :as_inserted_user
     test "renders form for editing chosen wallet", %{conn: conn, wallet: wallet} do
       conn = get(conn, Routes.wallet_path(conn, :edit, wallet))
       assert html_response(conn, 200) =~ "Edit Wallet"
@@ -70,6 +58,7 @@ defmodule XpendrWeb.WalletControllerTest do
   describe "update wallet" do
     setup [:create_wallet]
 
+    @tag :as_inserted_user
     test "redirects when data is valid", %{conn: conn, wallet: wallet} do
       conn = put(conn, Routes.wallet_path(conn, :update, wallet), wallet: @update_attrs)
       assert redirected_to(conn) == Routes.wallet_path(conn, :show, wallet)
@@ -78,6 +67,7 @@ defmodule XpendrWeb.WalletControllerTest do
       assert html_response(conn, 200) =~ "some updated description"
     end
 
+    @tag :as_inserted_user
     test "renders errors when data is invalid", %{conn: conn, wallet: wallet} do
       conn = put(conn, Routes.wallet_path(conn, :update, wallet), wallet: @invalid_attrs)
       assert html_response(conn, 200) =~ "Edit Wallet"
@@ -87,6 +77,7 @@ defmodule XpendrWeb.WalletControllerTest do
   describe "delete wallet" do
     setup [:create_wallet]
 
+    @tag :as_inserted_user
     test "deletes chosen wallet", %{conn: conn, wallet: wallet} do
       conn = delete(conn, Routes.wallet_path(conn, :delete, wallet))
       assert redirected_to(conn) == Routes.wallet_path(conn, :index)
@@ -97,8 +88,8 @@ defmodule XpendrWeb.WalletControllerTest do
     end
   end
 
-  defp create_wallet(_) do
-    wallet = fixture(:wallet)
+  defp create_wallet(%{user: user}) do
+    wallet = insert(:wallet, user: user)
     {:ok, wallet: wallet}
   end
 end
