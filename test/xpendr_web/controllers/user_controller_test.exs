185009e932a79d6fb964c6d827eb5bd6bd013b0b
João Fernandes <joaofnds@joaofnds.com>
2020-02-22 13:04:06 -0300

    Fix user_controller_test

--- test/xpendr_web/controllers/user_controller_test.exs
+++ test/xpendr_web/controllers/user_controller_test.exs
@@ -1,21 +1,15 @@
 defmodule XpendrWeb.UserControllerTest do
   use XpendrWeb.ConnCase, async: true
 
-  alias Xpendr.Accounts
-
-  @create_attrs %{name: "some name", username: "some username"}
+  @create_attrs %{name: "some name", username: "some username", password: "asdfasdfasdf"}
   @update_attrs %{name: "some updated name", username: "some updated username"}
   @invalid_attrs %{name: nil, username: nil}
 
-  def fixture(:user) do
-    {:ok, user} = Accounts.create_user(@create_attrs)
-    user
-  end
-
   describe "index" do
-    test "lists all users", %{conn: conn} do
-      conn = get(conn, Routes.user_path(conn, :index))
-      assert html_response(conn, 200) =~ "Listing Users"
+    test "doesn't exist", %{conn: conn} do
+      assert_error_sent 404, fn ->
+        get(conn, "/users")
+      end
     end
   end
 
@@ -27,6 +21,7 @@ defmodule XpendrWeb.UserControllerTest do
   end
 
   describe "create user" do
+    @tag :as_inserted_user
     test "redirects to show when data is valid", %{conn: conn} do
       conn = post(conn, Routes.user_path(conn, :create), user: @create_attrs)
 
@@ -37,6 +32,7 @@ defmodule XpendrWeb.UserControllerTest do
       assert html_response(conn, 200) =~ "Show User"
     end
 
+    @tag :as_inserted_user
     test "renders errors when data is invalid", %{conn: conn} do
       conn = post(conn, Routes.user_path(conn, :create), user: @invalid_attrs)
       assert html_response(conn, 200) =~ "New User"
@@ -44,8 +40,7 @@ defmodule XpendrWeb.UserControllerTest do
   end
 
   describe "edit user" do
-    setup [:create_user]
-
+    @tag :as_inserted_user
     test "renders form for editing chosen user", %{conn: conn, user: user} do
       conn = get(conn, Routes.user_path(conn, :edit, user))
       assert html_response(conn, 200) =~ "Edit User"
@@ -53,8 +48,7 @@ defmodule XpendrWeb.UserControllerTest do
   end
 
   describe "update user" do
-    setup [:create_user]
-
+    @tag :as_inserted_user
     test "redirects when data is valid", %{conn: conn, user: user} do
       conn = put(conn, Routes.user_path(conn, :update, user), user: @update_attrs)
       assert redirected_to(conn) == Routes.user_path(conn, :show, user)
@@ -63,6 +57,7 @@ defmodule XpendrWeb.UserControllerTest do
       assert html_response(conn, 200) =~ "some updated name"
     end
 
+    @tag :as_inserted_user
     test "renders errors when data is invalid", %{conn: conn, user: user} do
       conn = put(conn, Routes.user_path(conn, :update, user), user: @invalid_attrs)
       assert html_response(conn, 200) =~ "Edit User"
@@ -70,20 +65,14 @@ defmodule XpendrWeb.UserControllerTest do
   end
 
   describe "delete user" do
-    setup [:create_user]
-
+    @tag :as_inserted_user
     test "deletes chosen user", %{conn: conn, user: user} do
       conn = delete(conn, Routes.user_path(conn, :delete, user))
-      assert redirected_to(conn) == Routes.user_path(conn, :index)
+      assert redirected_to(conn) == Routes.page_path(conn, :index)
 
-      assert_error_sent 404, fn ->
-        get(conn, Routes.user_path(conn, :show, user))
+      assert_raise Ecto.NoResultsError, fn ->
+        Xpendr.Accounts.get_user!(user.id)
       end
     end
-  end
-
-  defp create_user(_) do
-    user = fixture(:user)
-    {:ok, user: user}
   end
 end
