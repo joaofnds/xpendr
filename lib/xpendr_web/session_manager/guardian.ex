227af21ffe636ec73915a2703ba548279d2d9e8e
João Fernandes <joaofnds@joaofnds.com>
2020-02-22 13:03:06 -0300

    Use `get_user` instead of `get_user!` on `resource_from_claims`
    With this, when the user is not found `current_user` claim will be nil

--- lib/xpendr_web/session_manager/guardian.ex
+++ lib/xpendr_web/session_manager/guardian.ex
@@ -6,6 +6,6 @@ defmodule XpendrWeb.SessionManager.Guardian do
   def subject_for_token(%Accounts.User{id: id}, _claims), do: {:ok, id}
   def subject_for_token(_, _), do: {:error, "unknown resource type"}
 
-  def resource_from_claims(%{"sub" => id}), do: {:ok, Accounts.get_user!(id)}
+  def resource_from_claims(%{"sub" => id}), do: {:ok, Accounts.get_user(id)}
   def resource_from_claims(_claims), do: {:error, "unknown resource type"}
 end
