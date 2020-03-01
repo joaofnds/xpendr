e553672f822e0542a9a974a1e039f83488b25241
João Fernandes <joaofnds@joaofnds.com>
2020-03-01 14:59:55 -0300

    Add Elm

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -31,6 +31,20 @@ module.exports = (env, options) => ({
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: {
+          loader: 'elm-webpack-loader',
+          options: {
+            cwd: path.resolve(__dirname, 'elm'),
+            pathToElm: path.resolve(__dirname, 'node_modules', '.bin', 'elm'),
+            debug: options.mode === 'development',
+            verbose: options.mode === 'development',
+            optimize: options.mode !== 'development'
+          }
+        }
       }
     ]
   },
